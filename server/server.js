08b624492d955518e68ef3b23dab62eec48b373e
Rehan-1119 <iaarehankhan2000@gmail.com>
2026-07-14 19:17:10 +0530

    Fix Gmail SMTP configuration

--- server/server.js
+++ server/server.js
@@ -33,7 +33,7 @@ app.get("/", (req, res) => {
    Server
 ================================ */
 
-const PORT = process.env.PORT || 7000;
+const PORT = process.env.PORT ;
 
 app.listen(PORT, () => {
 
